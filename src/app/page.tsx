1a5ae3cff95d21330a0d5088a003e2a75ff1be0c
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-04-19 08:16:50 +0530

    organised files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import HeroSection from "@/components/heroSection";
+import HeroSection from "@/sections/heroSection";
 import Services from "@/components/services";
 import PotentialEarnings from "@/components/potentialEarnings";
-import { Footer } from "@/components/footer";
+import { Footer } from "@/sections/footer";
 import Tape from "@/components/tape";
 import AffiliatePanel from "@/components/affiliatePanel";
 import { TestimonialsCarousel } from "@/components/testimonialsCarousel";
 import Cta from "@/components/cta";
-import FeatureSection from "@/components/featuresSection";
+import FeatureSection from "@/sections/featuresSection";
 import FAQSection from "@/components/faqSection";
 import WhySection from "@/components/whySection";
 
